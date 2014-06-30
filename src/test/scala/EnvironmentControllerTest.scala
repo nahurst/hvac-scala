e909d794e6388ee26d5349e587a0f05d6a4f457f
nahurst <nhurst@nhurst-mac.local>
2014-06-29 21:04:36 -0400

    Refactoring tests for easy reading

--- src/test/scala/EnvironmentControllerTest.scala
+++ src/test/scala/EnvironmentControllerTest.scala
@@ -1,238 +1,115 @@
 import org.scalatest.{Matchers, FlatSpec}
 
-/*
-Requirements:
-- if it's cold (less than 65 degrees) => heat
-- if it's hot (greater than 75 degrees) => cool
-- after the heater stops => run the fan for five more minutes
-- after cooler stops => don't turn it back on for three minutes
-
-use spy appropriately
-  will be a problem because we create hvac so many times
-  spy should allow writing temperature. others should not: use trait
-
-refactor tests
-
-how to fix the weird matcher "_ if"
-
-}
+/**
+ * Requirements:
+ * - if it's cold (less than 65 degrees) => heat
+ * - if it's hot (greater than 75 degrees) => cool
+ * - after the heater stops => run the fan for five more minutes
+ * - after cooler stops => don't turn it back on for three minutes
 */
 
 class HvacSpy(
-      override val heat: Boolean,
-      override val cool: Boolean,
-      override val fan: Boolean,
-      override val temperature: Double
+  override val heat: Boolean,
+  override val cool: Boolean,
+  override val fan: Boolean,
+  override val temperature: Double
 ) extends Hvac(heat, cool, fan, temperature)
+object HvacSpy {
+
+  def as(stateStr: String) = new HvacSpy(
+    stateStr.charAt(0).isUpper,
+    stateStr.charAt(1).isUpper,
+    stateStr.charAt(2).isUpper,
+    stateStr.substring(3).toInt)
+}
 
 
 class EnvironmentControllerTest extends FlatSpec with Matchers {
+
   "EnvironmentController" should "have tests" in {
     true should be === true
   }
 
+  var hvac: Hvac = HvacSpy.as("hcf70")
+  var envCntl: EnvironmentController = EnvironmentController(hvac)
+
   it should "heat when too cold" in {
     for (temperature <- 60 to 61) {
-      val hvac = new HvacSpy(false, false, false, temperature)
-      val envCntl = EnvironmentController(hvac)
-      val hvacResult = envCntl.tick()
-      hvacResult should have (
-        'heat (true),
-        'cool (false),
-        'fan  (true),
-        'temperature (temperature)
-      )
+      init("hcf" + temperature)
+      changeTempTickValidate(envCntl, "HcF", temperature)
     }
   }
 
   it should "cool when too hot" in {
     for (temperature <- 80 to 81) {
-      val hvac = new HvacSpy(false, false, false, temperature)
-      val envCntl = EnvironmentController(hvac)
-      val hvacResult = envCntl.tick()
-      hvacResult should have (
-        'heat (false),
-        'cool (true),
-        'fan  (true),
-        'temperature (temperature)
-      )
+      init("hcf" + temperature)
+      changeTempTickValidate(envCntl, "hCF", temperature)
     }
   }
 
-  it should "not heat or cool when in ideal range and it wasn't heating or cooling" in {
+  it should "not heat or cool when 65-75 and it wasn't heating or cooling" in {
     for (temperature <- List(65, 70, 75)) {
-      val hvac = new HvacSpy(false, false, false, temperature)
-      val envCntl = EnvironmentController(hvac)
-      val hvacResult = envCntl.tick()
-      hvacResult should have (
-        'heat (false),
-        'cool (false),
-        'fan  (false),
-        'temperature (temperature)
-      )
+      init("hcf" + temperature)
+      changeTempTickValidate(envCntl, "hcf", temperature)
     }
   }
 
-  it should "not heat or cool when in ideal range and it was heating" in {
+  it should "not heat or cool when 65-75 deg and it was heating" in {
     for (temperature <- List(65, 70, 75)) {
-      val hvac = new HvacSpy(true, false, false, temperature)
-      val envCntl = EnvironmentController(hvac)
-      val hvacResult = envCntl.tick()
-      hvacResult should have (
-        'heat (false),
-        'cool (false),
-        'fan  (false),
-        'temperature (temperature)
-      )
+      init("Hcf" + temperature)
+      changeTempTickValidate(envCntl, "hcf", temperature)
     }
   }
 
   it should "not heat or cool when 65-75 deg and it was cooling" in {
     for (temperature <- List(65, 70, 75)) {
-      val hvac = new HvacSpy(false, true, false, temperature)
-      val envCntl = EnvironmentController(hvac)
-      val hvacResult = envCntl.tick()
-      hvacResult should have (
-        'heat (false),
-        'cool (false),
-        'fan  (false),
-        'temperature (temperature)
-      )
+      init("hCf" + temperature)
+      changeTempTickValidate(envCntl, "hcf", temperature)
     }
   }
 
   it should "not run cooler if run within 3 minutes" in {
-    val hvac = new HvacSpy(false, false, false, 76)
-
-    var envCntl = EnvironmentController(hvac)
-    envCntl.tick() should have ( // 0 min
-      'heat (false),
-      'cool (true),
-      'fan  (true),
-      'temperature (76)
-    )
-
-    envCntl = envCntl.changeTemperature(74)
-    envCntl.tick() should have ( // 1 min
-      'heat (false),
-      'cool (false),
-      'fan  (false),
-      'temperature (74)
-    )
-
-    envCntl = envCntl.changeTemperature(76)
-    envCntl.tick() should have ( // 2 min
-      'heat (false),
-      'cool (false),
-      'fan  (false),
-      'temperature (76)
-    )
-
-    envCntl = envCntl.changeTemperature(76)
-    envCntl.tick() should have ( // 3 min
-      'heat (false),
-      'cool (true),
-      'fan  (true),
-      'temperature (76)
-    )
+    init("hcf76")
+    changeTempTickValidate(envCntl, "hCF", 76)
+    changeTempTickValidate(envCntl, "hcf", 74)
+    changeTempTickValidate(envCntl, "hcf", 76)
+    changeTempTickValidate(envCntl, "hCF", 76)
   }
 
-  // don't think this applies
-//  it should "continue running cooler if still too hot" in {
-//    val hvac = new HvacSpy(false, false, false, 76)
-//    var envCntl = EnvironmentController(hvac)
-//    envCntl.tick() should have (
-//      'heat (false),
-//      'cool (true),
-//      'fan  (true),
-//      'temperature (76)
-//    )
-//
-//    envCntl = envCntl.changeTemperature(76)
-//    envCntl.tick() should have (
-//      'heat (false),
-//      'cool (true),
-//      'fan  (true),
-//      'temperature (76)
-//    )
-//  }
-
   it should "run fan for 5 minutes after heating" in {
-    val hvac = new HvacSpy(false, false, false, 64)
-    var envCntl = EnvironmentController(hvac)
-
-    envCntl.tick() should have ( // 0 min
-      'heat (true),
-      'cool (false),
-      'fan  (true),
-      'temperature (64)
-    )
-
-    for (i <- 1 to 4) {
-      envCntl = envCntl.changeTemperature(66)
-      envCntl.tick() should have ( // 1-4 min
-        'heat (false),
-        'cool (false),
-        'fan  (true),
-        'temperature (66)
-      )
-    }
-
-    envCntl = envCntl.changeTemperature(66)
-    envCntl.tick() should have ( // 1-4 min
-      'heat (false),
-      'cool (false),
-      'fan  (false),
-      'temperature (66)
-    )
+    init("hcf64")
+    changeTempTickValidate(envCntl, "HcF", 64)
+    changeTempTickValidate(envCntl, "hcF", 66)
+    changeTempTickValidate(envCntl, "hcF", 66)
+    changeTempTickValidate(envCntl, "hcF", 66)
+    changeTempTickValidate(envCntl, "hcF", 66)
+    changeTempTickValidate(envCntl, "hcf", 66)
   }
 
   it should "continue to run fan after heating even if cooling soon after" in {
-    val hvac = new HvacSpy(false, false, false, 64)
-    var envCntl = EnvironmentController(hvac)
-
-    envCntl.tick() should have ( // 0 min
-      'heat (true),
-      'cool (false),
-      'fan  (true),
-      'temperature (64)
-    )
-
-    envCntl = envCntl.changeTemperature(66)
-    envCntl.tick() should have ( // 1 min
-      'heat (false),
-      'cool (false),
-      'fan  (true),
-      'temperature (66)
-    )
-
-    envCntl = envCntl.changeTemperature(76)
-    envCntl.tick() should have ( // 2 min
-      'heat (false),
-      'cool (true),
-      'fan  (true),
-      'temperature (76)
-    )
-
-    for (i <- 3 to 4) {
-      envCntl = envCntl.changeTemperature(74)
-      envCntl.tick() should have ( // 3-4 min
-        'heat (false),
-        'cool (false),
-        'fan  (true),
-        'temperature (74)
-      )
-    }
-
-    envCntl = envCntl.changeTemperature(74)
-    envCntl.tick() should have ( // 5 min
-      'heat (false),
-      'cool (false),
-      'fan  (false),
-      'temperature (74)
-    )
-
+    init("hcf64")
+    changeTempTickValidate(envCntl, "HcF", 64)
+    changeTempTickValidate(envCntl, "hcF", 66)
+    changeTempTickValidate(envCntl, "hCF", 76)
+    changeTempTickValidate(envCntl, "hcF", 74)
+    changeTempTickValidate(envCntl, "hcF", 74)
+    changeTempTickValidate(envCntl, "hcf", 74)
   }
 
+  def init(stateStr: String) {
+    hvac = HvacSpy.as(stateStr)
+    envCntl = EnvironmentController(hvac)
+  }
+
+  def changeTempTickValidate(envCntl:EnvironmentController, stateStr: String, temperature: Int) = {
+    envCntl.changeTemperature(temperature).tick() shouldEqual HvacSpy.as(stateStr + temperature)
+  }
+
+  // don't think this applies
+  //  it should "continue running cooler if still too hot" in {
+  //    init("hcf76")
+  //    changeTempTickValidate(envCntl, "hCF", 76)
+  //    changeTempTickValidate(envCntl, "hCF", 76)
+  //  }
 
 }
